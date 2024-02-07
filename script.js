a2799a21d98a8c9eb8a1ab7f854c7510419e91b9
Michal Kubiče <kubicemichal@gmail.com>
2024-02-07 13:29:09 +0200

    fix cube

--- script.js
+++ script.js
@@ -14,23 +14,56 @@ const size = 100,
   yPosition = -100,
   zPosition = -500; // Center position of the cube
 var map = [
-		   [0,0,1000,0,180,0,2000,600,"url('textures/1.jpg')", "ff0000"],
-		   [0,0,-1000,0,0,0,2000,600,"url('textures/1.jpg')", "ff0000"],
-		   [1000,0,0,0,-90,0,2000,600,"url('textures/1.jpg')", "ff0000"],
-		   [-1000,0,0,0,90,0,2000,600,"url('textures/1.jpg')", "ff0000"],
-		   [0,100,0,90,0,0,2000,2000,"url('textures/2.jpg')", "00ff00"],
+  [0, 0, 1000, 0, 180, 0, 2000, 600, "url('textures/1.jpg')", "ff0000"],
+  [0, 0, -1000, 0, 0, 0, 2000, 600, "url('textures/1.jpg')", "ff0000"],
+  [1000, 0, 0, 0, -90, 0, 2000, 600, "url('textures/1.jpg')", "ff0000"],
+  [-1000, 0, 0, 0, 90, 0, 2000, 600, "url('textures/1.jpg')", "ff0000"],
+  [0, 100, 0, 90, 0, 0, 2000, 2000, "url('textures/2.jpg')", "00ff00"],
   // Top Face
-  [xPosition, yPosition - 50, zPosition, 90, 0, 0, size, size, "", "#FF0000"],
+  [xPosition, yPosition - 50, zPosition, 90, 0, 0, size, size, null, "#FF0000"],
   // Bottom Face
-  [xPosition, yPosition + 50, zPosition, -90, 0, 0, size, size, "", "#00FF00"],
+  [
+    xPosition,
+    yPosition + 50,
+    zPosition,
+    -90,
+    0,
+    0,
+    size,
+    size,
+    null,
+    "#00FF00",
+  ],
   // Front Face
-  [xPosition, yPosition, zPosition - 50, 0, 0, 0, size, size, "", "#0000FF"],
+  [xPosition, yPosition, zPosition - 50, 0, 0, 0, size, size, null, "#0000FF"],
   // Back Face
-  [xPosition, yPosition, zPosition + 50, 0, 180, 0, size, size, "", "#FFFF00"],
+  [
+    xPosition,
+    yPosition,
+    zPosition + 50,
+    0,
+    180,
+    0,
+    size,
+    size,
+    null,
+    "#FFFF00",
+  ],
   // Right Face
-  [xPosition + 50, yPosition, zPosition, 0, 90, 0, size, size, "", "#00FFFF"],
+  [xPosition + 50, yPosition, zPosition, 0, 90, 0, size, size, null, "#00FFFF"],
   // Left Face
-  [xPosition - 50, yPosition, zPosition, 0, -90, 0, size, size, "", "#FF00FF"],
+  [
+    xPosition - 50,
+    yPosition,
+    zPosition,
+    0,
+    -90,
+    0,
+    size,
+    size,
+    null,
+    "#FF00FF",
+  ],
 ];
 
 var PressBack = 0;
@@ -141,33 +174,42 @@ function update() {
     "px)";
 }
 
-function CreateNewWorld(){
-	for (let i = 0; i < map.length; i++){
-		
-		let newElement = document.createElement("div");
-		newElement.className = "square";
-		newElement.id = "square" + i;
-		newElement.style.width = map[i][6] + "px";
-		newElement.style.height = map[i][7] + "px";
-		
-		if (!map[i][8] == null) {
-			newElement.style.background = map[i][9];
-		} else {
-			newElement.style.backgroundImage = map[i][8];
-		}
-		
-		console.log(map[i][8])
-		
-		newElement.style.transform = "translate3d(" +
-		(600 - map[i][6]/2 + map[i][0]) + "px," +
-		(400 - map[i][7]/2 + map[i][1]) + "px," +
-		                    (map[i][2]) + "px)" +
-		"rotateX(" + map[i][3] + "deg)" +
-		"rotateY(" + map[i][4] + "deg)" +
-		"rotateZ(" + map[i][5] + "deg)";
-		
-		world.append(newElement);
-	}
+function CreateNewWorld() {
+  for (let i = 0; i < map.length; i++) {
+    let newElement = document.createElement("div");
+    newElement.className = "square";
+    newElement.id = "square" + i;
+    newElement.style.width = map[i][6] + "px";
+    newElement.style.height = map[i][7] + "px";
+
+    if (map[i][8] == null) {
+      newElement.style.background = map[i][9];
+    } else {
+      newElement.style.backgroundImage = map[i][8];
+    }
+
+    console.log(map[i][8]);
+
+    newElement.style.transform =
+      "translate3d(" +
+      (600 - map[i][6] / 2 + map[i][0]) +
+      "px," +
+      (400 - map[i][7] / 2 + map[i][1]) +
+      "px," +
+      map[i][2] +
+      "px)" +
+      "rotateX(" +
+      map[i][3] +
+      "deg)" +
+      "rotateY(" +
+      map[i][4] +
+      "deg)" +
+      "rotateZ(" +
+      map[i][5] +
+      "deg)";
+
+    world.append(newElement);
+  }
 }
 
 CreateNewWorld();
