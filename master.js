4423274ea9964ce0431236cf738a56d6e407fd66
Patric <pmorevs@gmail.com>
2024-02-21 14:44:34 +0200

    Added items and restructurized the project

---
+++ master.js
@@ -0,0 +1,15 @@
+function include(file) {
+
+	let script = document.createElement('script');
+	script.src = file;
+	script.type = 'text/javascript';
+	script.defer = true;
+
+	document.getElementsByTagName('head').item(0).appendChild(script);
+
+}
+
+/* Include Many js files */
+include('map.js');
+include('items.js');
+include('script.js');
--- script.js
+++ script.js
@@ -9,80 +9,6 @@ function player(x, y, z, rx, ry) {
   this.ry = ry;
 }
 
-const size = 100,
-  xPosition = -100,
-  yPosition = -100,
-  zPosition = -500; // Center position of the cube
-var map = [
-  [0, 0, 1000, 0, 180, 0, 2000, 600, "url('textures/1.jpg')", "ff0000"],
-  [0, 0, -1000, 0, 0, 0, 2000, 600, "url('textures/1.jpg')", "ff0000"],
-  [1000, 0, 0, 0, -90, 0, 2000, 600, "url('textures/1.jpg')", "ff0000"],
-  [-1000, 0, 0, 0, 90, 0, 2000, 600, "url('textures/1.jpg')", "ff0000"],
-  [0, 100, 0, 90, 0, 0, 2000, 2000, "url('textures/2.jpg')", "00ff00"],
-  //Patric's wall
-  [500, 0, 580, 0, 0, 0, 1000, 600, "url('textures/1.jpg')", "ff0000"],
-  [500, 0, 600, 0, 0, 0, 1000, 600, "url('textures/1.jpg')", "ff0000"],
-  [0, 0, 590, 0, 90, 0, 20, 600, "url('textures/1.jpg')", "ff0000"],
-  //Altyshka's wall
-  [-500, 0, 500, 0, 90, 0, 1000, 600, "url('textures/1.jpg')", "ff0000"],
-  [-480, 0, 500, 0, 90, 0, 1000, 600, "url('textures/1.jpg')", "ff0000"],
-  [-490, 0, 0, 0, 0, 0, 20, 600, "url('textures/1.jpg')", "ff0000"],
-  //Antelz's wall
-  [680,0,-500, 0, 90, 0, 1000, 600, "url('textures/1.jpg')", "ff0000"],
-  [700, 0,-500, 0, 90, 0, 1000, 600, "url('textures/1.jpg')", "ff0000"],
-  [690, 0,0, 0, 0, 0, 20, 600, "url('textures/1.jpg')", "ff0000"],
-  //Adheeksha's wall
-  [-500, 0, -580, 0, 0, 0, 1000, 600, "url('textures/1.jpg')", "ff0000"],
-  [-500, 0, -600, 0, 0, 0, 1000, 600, "url('textures/1.jpg')", "ff0000"],
-  [0, 0, -590, 0, 90, 0, 20, 600, "url('textures/1.jpg')", "ff0000"],
-
-  // Top Face
-  [xPosition, yPosition - 50, zPosition, 90, 0, 0, size, size, null, "#FF0000"],
-  // Bottom Face
-  [
-    xPosition,
-    yPosition + 50,
-    zPosition,
-    -90,
-    0,
-    0,
-    size,
-    size,
-    null,
-    "#00FF00",
-  ],
-  // Front Face
-  [xPosition, yPosition, zPosition - 50, 0, 0, 0, size, size, null, "#4d0000"],
-  // Back Face
-  [
-    xPosition,
-    yPosition,
-    zPosition + 50,
-    0,
-    180,
-    0,
-    size,
-    size,
-    null,
-    "#FFFFFF",
-  ],
-  // Right Face
-  [xPosition + 50, yPosition, zPosition, 0, 90, 0, size, size, null, "#00FFFF"],
-  // Left Face
-  [
-    xPosition - 50,
-    yPosition,
-    zPosition,
-    0,
-    -90,
-    0,
-    size,
-    size,
-    null,
-    "#0F7209",
-  ],
-];
-
 var PressBack = 0;
 var PressForward = 0;
 var PressLeft = 0;
@@ -202,43 +128,6 @@ function update() {
     "px)";
 }
 
-function CreateNewWorld() {
-  for (let i = 0; i < map.length; i++) {
-    let newElement = document.createElement("div");
-    newElement.className = "square";
-    newElement.id = "square" + i;
-    newElement.style.width = map[i][6] + "px";
-    newElement.style.height = map[i][7] + "px";
-
-    if (map[i][8] == null) {
-      newElement.style.background = map[i][9];
-    } else {
-      newElement.style.backgroundImage = map[i][8];
-    }
-
-    console.log(map[i][8]);
-
-    newElement.style.transform =
-      "translate3d(" +
-      (600 - map[i][6] / 2 + map[i][0]) +
-      "px," +
-      (400 - map[i][7] / 2 + map[i][1]) +
-      "px," +
-      map[i][2] +
-      "px)" +
-      "rotateX(" +
-      map[i][3] +
-      "deg)" +
-      "rotateY(" +
-      map[i][4] +
-      "deg)" +
-      "rotateZ(" +
-      map[i][5] +
-      "deg)";
-
-    world.append(newElement);
-  }
-}
-
 CreateNewWorld();
+createItems();
 TimerGame = setInterval(update, 10);
