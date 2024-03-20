8bfc6640a8f7e3f346a27a8338adcce5e2b29829
AdheekshaRajapaksha <preshaleerajapaksha@gmail.com>
2024-03-20 15:36:02 +0200

    teleport angle and position changed Adheeksha's map

--- mapAdheeksha.js
+++ mapAdheeksha.js
@@ -5,7 +5,7 @@ var mapAdheeksha = [
     [-1500, 0, 0, 0, 90, 0, 3000, 1200, "url('textures/mapAdheeksha1.jpg')", "ff0000"],
     [0, 100, 0, 90, 0, 0, 3000, 3000, "url('textures/mapAdheeksha2.jpg')", "00ff00"], //floor
 
-    [-1300,100,1300, 90, 0, 0, 300, 300, "url('textures/teleportAdheeksha.gif')", "00ff00"], //hole
+    [-1300,-70,1300, 10, 0, 90, 300, 300, "url('textures/teleportAdheeksha.gif')", "00ff00"], //hole
     //Patric's wall
     [750, 0, 1190, 0, 0, 0, 1500, 1200, "url('textures/mapAdheeksha3.jpg')", "ff0000"],
     [750, 0, 1200, 0, 0, 0, 1500, 1200, "url('textures/mapAdheeksha3.jpg')", "ff0000"],
